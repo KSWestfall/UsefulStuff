5caea8a522b30d35a85c600899131a4e463517f1
Kyle Westfall <kyle.westfall@pressganey.com>
2017-07-20 08:30:45 -0400

    add other useful ps scripts remove useless ones update vssettings

---
+++ Powershell Scripts/notification.ps1
@@ -0,0 +1,19 @@
+﻿[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+
+$objNotifyIcon = New-Object System.Windows.Forms.NotifyIcon 
+
+$objNotifyIcon.Icon = "C:\utils\Folder.ico"
+$objNotifyIcon.BalloonTipIcon = "Info" 
+$objNotifyIcon.BalloonTipText = "Retrieving files from C:\Windows." 
+$objNotifyIcon.BalloonTipTitle = "Retrieving Files" 
+
+$objNotifyIcon.Visible = $True 
+$objNotifyIcon.ShowBalloonTip(10000)
+
+Get-ChildItem C:\Windows
+
+$objNotifyIcon.BalloonTipText = "The script has finished running." 
+$objNotifyIcon.BalloonTipTitle = "Files retrieved." 
+
+$objNotifyIcon.Visible = $True 
+$objNotifyIcon.ShowBalloonTip(10000)
